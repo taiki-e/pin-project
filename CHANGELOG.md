6bb6b7d464d3272c038160ca5ba0a7ba75f67858
Taiki Endo <te316e89@gmail.com>
2019-01-09 00:30:12 +0900

    Release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Unreleased
 
+# 0.1.1 - 2019-01-08
+
+* Rename from `unsafe_pin_project` to `unsafe_project`
+
 # 0.1.0 - 2019-01-08
 
 Initial release
--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@
 
 #![crate_type = "proc-macro"]
 #![recursion_limit = "256"]
-#![doc(html_root_url = "https://docs.rs/pin-project/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/pin-project/0.1.1")]
 
 extern crate proc_macro;
 
