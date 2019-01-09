00b3e8f620ce75bc6b09f96184504c4b0fb70e54
Taiki Endo <te316e89@gmail.com>
2019-01-09 17:12:51 +0900

    Improve documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # Unreleased
 
+* Improve documentation
+
 # 0.1.1 - 2019-01-08
 
 * Rename from `unsafe_pin_project` to `unsafe_project`
--- src/lib.rs
+++ src/lib.rs
@@ -25,6 +25,10 @@
 //! impl<T: Unpin, U> Unpin for Foo<T, U> {} // Conditional Unpin impl
 //! ```
 //!
+//! See [`unsafe_project`] for more details.
+//!
+//! [`unsafe_project`]: ./attr.unsafe_project.html
+//!
 //! ## Rust Version
 //!
 //! The current version of pin-project requires Rust nightly 2018-12-26 or later.
@@ -42,6 +46,54 @@ use quote::{quote, ToTokens};
 use syn::{Attribute, Field, Fields, FieldsNamed, Ident, ItemStruct};
 
 /// An attribute that would create a projection struct covering all the fields.
+///
+/// For the field that use `#[pin]` attribute, makes the pinned reference to the field.
+///
+/// For the other field, makes the unpinned reference to the field.
+///
+/// ## Safety
+///
+/// For the field that use `#[pin]` attribute, three things need to be ensured:
+///
+/// - If the struct implements [`Drop`], the [`drop`] method is not allowed to
+///   move the value of the field.
+/// - If the struct wants to implement [`Unpin`], it has to do so conditionally:
+///   The struct can only implement [`Unpin`] if the field's type is [`Unpin`].
+/// - The struct must not be `#[repr(packed)]`.
+///
+/// For the other field, need to be ensured that the contained value not pinned in
+/// the current context.
+///
+/// ## Examples
+///
+/// ```rust
+/// use pin_project::unsafe_project;
+/// use std::marker::Unpin;
+/// use std::pin::Pin;
+///
+/// #[unsafe_project]
+/// struct Foo<T, U> {
+///     #[pin]
+///     future: T,
+///     field: U,
+/// }
+///
+/// impl<T, U> Foo<T, U> {
+///     fn baz(mut self: Pin<&mut Self>) {
+///         let this = self.project();
+///         let _: Pin<&mut T> = this.future; // Pinned reference to the field
+///         let _: &mut U = this.field; // Normal reference to the field
+///     }
+/// }
+///
+/// impl<T: Unpin, U> Unpin for Foo<T, U> {} // Conditional Unpin impl
+/// ```
+///
+/// Note that borrowing the field where `#[pin]` attribute is used multiple
+/// times requires using `.as_mut()` to avoid consuming the `Pin`.
+///
+/// [`Unpin`]: core::marker::Unpin
+/// [`drop`]: Drop::drop
 #[proc_macro_attribute]
 pub fn unsafe_project(args: TokenStream, input: TokenStream) -> TokenStream {
     if !args.is_empty() {
