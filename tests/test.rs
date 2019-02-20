ac976e2c4f5803be7a8f3f2f16976ac55dfc7386
Taiki Endo <te316e89@gmail.com>
2019-02-20 23:15:04 +0900

    Remove unsafe_fields attribute

--- tests/test.rs
+++ tests/test.rs
@@ -248,59 +248,6 @@ fn test_project_attr() {
     }
 }
 
-#[cfg(feature = "unsafe_fields")]
-#[test]
-fn test_unsafe_fields() {
-    use pin_project::unsafe_fields;
-
-    #[unsafe_fields(Unpin)]
-    struct Foo<T, U> {
-        #[pin]
-        field1: T,
-        field2: U,
-    }
-
-    let mut foo = Foo {
-        field1: 1,
-        field2: 2,
-    };
-
-    let mut foo = Pin::new(&mut foo);
-
-    let x: Pin<&mut i32> = foo.as_mut().field1();
-    assert_eq!(*x, 1);
-
-    let y: &mut i32 = foo.as_mut().field2();
-    assert_eq!(*y, 2);
-
-    // skip
-
-    #[unsafe_fields(Unpin)]
-    struct Bar<T, U> {
-        #[pin]
-        field1: T,
-        field2: U,
-        #[skip]
-        _field3: (),
-    }
-
-    let mut bar = Bar {
-        field1: 1,
-        field2: 2,
-        _field3: (),
-    };
-
-    let mut bar = Pin::new(&mut bar);
-
-    let x: Pin<&mut i32> = bar.as_mut().field1();
-    assert_eq!(*x, 1);
-
-    let y: &mut i32 = bar.as_mut().field2();
-    assert_eq!(*y, 2);
-
-    // let _z = bar.as_mut()._field3(); // ERROR
-}
-
 #[cfg(feature = "unsafe_variants")]
 #[test]
 fn test_unsafe_variants() {
