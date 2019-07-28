87668a9f00c97569417ed225a2f11aa0d7877590
Aaron Hill <aa1ronham@gmail.com>
2019-07-28 13:28:43 -0400

    Test that the relative ordering of `#[pin_projectable]` and `#[repr(packed)]` doesn't matter

--- pin-project/tests/ui/unsafe_project/packed.rs
+++ pin-project/tests/ui/unsafe_project/packed.rs
@@ -10,9 +10,23 @@ struct Foo {
     field: u8
 }
 
+// Test putting 'repr' before the 'pin_projectable' attribute
+#[repr(packed, C)] //~ ERROR may not be used on #[repr(packed)] type
+#[pin_projectable]
+struct Foo2 {
+    #[pin]
+    field: u8
+}
+
 #[pin_projectable]
 #[repr(packed, C)] //~ ERROR may not be used on #[repr(packed)] type
 enum Blah {
+    Tuple(#[pin] u8)
+}
+
+#[repr(packed, C)] //~ ERROR may not be used on #[repr(packed)] type
+#[pin_projectable]
+enum Blah2 {
     Tuple(#[pin] u8)
 }
 
