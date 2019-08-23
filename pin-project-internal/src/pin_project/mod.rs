68461ff8bb72d5d86d905226846d3e03715f471e
Taiki Endo <te316e89@gmail.com>
2019-08-23 15:53:22 +0900

    Avoid clippy::drop_bounds lint in generated code

--- pin-project-internal/src/pin_project/mod.rs
+++ pin-project-internal/src/pin_project/mod.rs
@@ -281,6 +281,7 @@ impl<'a> ImplDrop<'a> {
                 // which will then comflict with the explicit MustNotImplDrop impl below.
                 // This will result in a compilation error, which is exactly what we want
                 trait #trait_ident {}
+                #[allow(clippy::drop_bounds)]
                 impl<T: ::core::ops::Drop> #trait_ident for T {}
                 impl #impl_generics #trait_ident for #ident #ty_generics #where_clause {}
             }
