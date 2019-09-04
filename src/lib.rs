6a535788709cb084a515b7e3b841b467a3b9f624
Aaron Hill <aa1ronham@gmail.com>
2019-09-03 20:47:58 -0400

    Fix clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -167,15 +167,15 @@ use core::pin::Pin;
 /// while preserving lifetimes.
 ///
 /// Normally, `Pin::as_mut` can be used to convert a '&mut Pin' to a owned 'Pin'
-/// However, since 'Pin::as_mut' needs to work with generic DerefMut impls,
-/// it loses lifetime information. Specifically, it returns a 'Pin<&'a mut P::Target>',
+/// However, since `Pin::as_mut'`needs to work with generic `DerefMut` impls,
+/// it loses lifetime information. Specifically, it returns a `Pin<&'a mut P::Target>`,
 /// where 'a is tied to the lifetime of the original '&mut Pin'. If you started
 /// with a `&mut Pin<'pin &mut T>`, the 'pin lifetime is lost.
 ///
 /// This can cause issues when trying to return the result of a pin projection
 /// from a method - e.g. `fn foo<'a>(mut self: Pin<&'a mut Self>) -> Pin<&'a mut T>`
 ///
-/// The ProjectThrough trait was introduced to solve this issue.
+/// The `ProjectThrough` trait was introduced to solve this issue.
 /// Normally, you'll never need to interact with this type
 /// directly - it's used internally by pin-project to allow expressing
 /// the proper lifetime.
