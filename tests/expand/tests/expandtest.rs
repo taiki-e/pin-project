952983ade30a49cc56e09f2e500856af7c568c61
Taiki Endo <te316e89@gmail.com>
2020-05-11 01:26:05 +0900

    Fix expandtest

--- tests/expand/tests/expandtest.rs
+++ tests/expand/tests/expandtest.rs
@@ -4,5 +4,9 @@
 #[cfg_attr(any(not(cargo_expand), all(ci, not(target_os = "linux"))), ignore)]
 #[test]
 fn expandtest() {
+    #[cfg(target_os = "linux")] // FIXME
+    #[cfg(ci)]
+    macrotest::expand_without_refresh("tests/expand/*.rs");
+    #[cfg(not(ci))]
     macrotest::expand("tests/expand/*.rs");
 }
