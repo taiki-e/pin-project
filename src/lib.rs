91d3aa1f115251e8a342a621975579062ba4d4ed
Taiki Endo <te316e89@gmail.com>
2020-05-11 09:42:48 +0900

    Release 0.4.16

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@
 
 #![no_std]
 #![recursion_limit = "256"]
-#![doc(html_root_url = "https://docs.rs/pin-project/0.4.15")]
+#![doc(html_root_url = "https://docs.rs/pin-project/0.4.16")]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms, single_use_lifetimes), allow(dead_code))
