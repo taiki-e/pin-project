85394efe8c62012e138355e9e87486d9975c786b
Taiki Endo <te316e89@gmail.com>
2020-05-18 04:19:34 +0900

    Cleanup CI config

--- tests/expand/tests/expandtest.rs
+++ tests/expand/tests/expandtest.rs
@@ -1,10 +1,8 @@
-#![cfg(nightly)]
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 
-#[cfg_attr(any(not(cargo_expand), all(ci, not(target_os = "linux"))), ignore)]
+#[cfg_attr(not(all(cargo_expand, nightly)), ignore)]
 #[test]
 fn expandtest() {
-    #[cfg(target_os = "linux")] // FIXME
     #[cfg(ci)]
     macrotest::expand_without_refresh("tests/expand/*.rs");
     #[cfg(not(ci))]
