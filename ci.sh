6caecae5adf7ec45f3803aea503d55cac78394bd
Taiki Endo <te316e89@gmail.com>
2020-07-13 18:21:23 +0900

    Use TRYBUILD=overwrite in ci.sh

--- ci.sh
+++ ci.sh
@@ -15,7 +15,7 @@ echo "Running 'cargo clippy'"
 cargo +nightly clippy --all --all-features --all-targets
 
 echo "Running 'cargo test'"
-cargo +nightly test --all --all-features --exclude expandtest
+TRYBUILD=overwrite cargo +nightly test --all --all-features --exclude expandtest
 
 echo "Running 'cargo doc'"
 cargo +nightly doc --no-deps --all --all-features
