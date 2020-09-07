b15c50b6bdfd6920c650ffe060255a768da37ead
Taiki Endo <te316e89@gmail.com>
2020-09-07 15:52:52 +0900

    Ignore clippy::match_like_matches_macro lint
    matches! requires Rust 1.42

--- src/lib.rs
+++ src/lib.rs
@@ -43,8 +43,12 @@
 ))]
 #![warn(missing_docs, rust_2018_idioms, single_use_lifetimes, unreachable_pub)]
 #![warn(clippy::all, clippy::default_trait_access)]
-// mem::take and #[non_exhaustive] requires Rust 1.40
-#![allow(clippy::mem_replace_with_default, clippy::manual_non_exhaustive)]
+// mem::take and #[non_exhaustive] requires Rust 1.40, matches! requires Rust 1.42
+#![allow(
+    clippy::mem_replace_with_default,
+    clippy::manual_non_exhaustive,
+    clippy::match_like_matches_macro
+)]
 #![allow(clippy::needless_doctest_main)]
 
 #[doc(inline)]
