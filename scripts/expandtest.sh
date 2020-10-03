401b13631d689ee690cf45108247cb2f26e17f0a
Taiki Endo <te316e89@gmail.com>
2020-10-03 18:41:37 +0900

    Cleanup scripts

--- scripts/expandtest.sh
+++ scripts/expandtest.sh
@@ -11,8 +11,14 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${0}")" && pwd)"
 
 if [[ "${CI:-false}" != "true" ]]; then
-    # See also https://docs.rs/macrotest/1/macrotest/#updating-expandedrs
+    toolchain="+nightly"
+
+    # First, check if the compile fails for another reason.
+    cargo ${toolchain} check --tests --manifest-path "${script_dir}"/../tests/expand/Cargo.toml
+
+    # Next, remove the `*.expanded.rs` files to allow updating those files.
+    # Refs: https://docs.rs/macrotest/1/macrotest/#updating-expandedrs
     rm -rf "${script_dir}"/../tests/expand/tests/expand/*.expanded.rs
 fi
 
-cargo +nightly test --manifest-path "${script_dir}"/../tests/expand/Cargo.toml
+cargo ${toolchain:-} test --manifest-path "${script_dir}"/../tests/expand/Cargo.toml
