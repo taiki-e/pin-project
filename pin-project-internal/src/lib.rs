322e8d21c66154e6164348cfd4a533b8c91e9f59
Taiki Endo <te316e89@gmail.com>
2020-10-13 23:23:23 +0900

    Release 1.0.0

--- pin-project-internal/src/lib.rs
+++ pin-project-internal/src/lib.rs
@@ -1,6 +1,6 @@
 //! Implementation detail of the `pin-project` crate. - **do not use directly**
 
-#![doc(html_root_url = "https://docs.rs/pin-project-internal/1.0.0-alpha.1")]
+#![doc(html_root_url = "https://docs.rs/pin-project-internal/1.0.0")]
 #![doc(test(
     no_crate_inject,
     attr(deny(warnings, rust_2018_idioms, single_use_lifetimes), allow(dead_code))
@@ -472,7 +472,7 @@ use proc_macro::TokenStream;
 /// [`Pin::as_mut`]: core::pin::Pin::as_mut
 /// [`Pin::set`]: core::pin::Pin::set
 /// [`Pin`]: core::pin::Pin
-/// [`UnsafeUnpin`]: https://docs.rs/pin-project/1.0.0-alpha.1/pin_project/trait.UnsafeUnpin.html
+/// [`UnsafeUnpin`]: https://docs.rs/pin-project/1/pin_project/trait.UnsafeUnpin.html
 /// [drop-guarantee]: core::pin#drop-guarantee
 /// [pin-projection]: core::pin#projections-and-structural-pinning
 /// [pinned-drop]: macro@pin_project#pinned_drop
