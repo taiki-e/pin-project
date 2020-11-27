610ba23b9b66f5e2017e1307e181035e96f9e2b8
Taiki Endo <te316e89@gmail.com>
2020-11-27 11:01:50 +0900

    Update lint list to nightly-2020-11-27

--- tests/lint.rs
+++ tests/lint.rs
@@ -1025,7 +1025,7 @@ pub mod clippy_used_underscore_binding {
 #[cfg(not(miri))]
 #[allow(box_pointers)]
 #[allow(clippy::restriction)]
-#[rustversion::attr(before(2020-11-20), ignore)] // Note: This date is commit-date and the day before the toolchain date.
+#[rustversion::attr(before(2020-11-26), ignore)] // Note: This date is commit-date and the day before the toolchain date.
 #[test]
 fn check_lint_list() {
     use std::{env, fs, path::Path, process::Command, str};
