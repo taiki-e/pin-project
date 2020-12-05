ad568dc8bd61cb09a6b29d41ca27e437a2310a89
Taiki Endo <te316e89@gmail.com>
2020-12-05 17:09:45 +0900

    Add #[track_caller] to assertion helper

--- tests/auxiliary/mod.rs
+++ tests/auxiliary/mod.rs
@@ -16,6 +16,7 @@ macro_rules! assert_not_unpin {
     };
 }
 
+#[rustversion::attr(since(1.46), track_caller)]
 pub fn assert_diff(expected_path: impl AsRef<Path>, actual: impl AsRef<str>) {
     let actual = actual.as_ref();
     let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
