573d3e8768a5c567676a4ae5be1774a5029b8975
Taiki Endo <te316e89@gmail.com>
2021-01-12 18:09:15 +0900

    Merge expandtest into main crate

--- tests/expandtest.rs
+++ tests/expandtest.rs
@@ -1,3 +1,4 @@
+#![cfg(not(miri))]
 #![warn(rust_2018_idioms, single_use_lifetimes)]
 
 use std::{
@@ -5,22 +6,25 @@ use std::{
     process::{Command, ExitStatus, Stdio},
 };
 
-fn main() {
-    if !is_nightly() {
+#[rustversion::attr(not(nightly), ignore)]
+#[test]
+fn expandtest() {
+    let is_ci = env::var_os("CI").is_some();
+    let cargo = &*env::var("CARGO").unwrap_or_else(|_| "cargo".into());
+    if !has_command(&[cargo, "expand"]) || !has_command(&[cargo, "fmt"]) {
+        if is_ci {
+            panic!("expandtest requires rustfmt and cargo-expand")
+        }
         return;
     }
 
-    let cargo = &*env::var("CARGO").unwrap_or_else(|_| "cargo".into());
-    if !has_command(&[cargo, "expand"]) || !has_command(&[cargo, "fmt"]) {
-        println!("cargo:warning=rustfmt or cargo-expand not found, skipping expandtest");
+    let path = "tests/expand/*/*.rs";
+    if is_ci {
+        macrotest::expand_without_refresh(path);
+    } else {
+        env::set_var("MACROTEST", "overwrite");
+        macrotest::expand(path);
     }
-}
-
-fn is_nightly() -> bool {
-    env::var_os("RUSTC")
-        .and_then(|rustc| Command::new(rustc).arg("--version").output().ok())
-        .and_then(|output| String::from_utf8(output.stdout).ok())
-        .map_or(false, |version| version.contains("nightly") || version.contains("dev"))
 }
 
 fn has_command(command: &[&str]) -> bool {
