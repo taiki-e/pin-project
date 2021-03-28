534235ca913ff152840668a0c7b2624631f5f1d3
Taiki Endo <te316e89@gmail.com>
2021-03-28 09:06:18 +0900

    Update configuration files based on main branch

---
+++ tools/ci.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Run a simplified version of the checks done by CI.
+#
+# Usage:
+#     ./tools/ci.sh [+toolchain]
+#
+# Note: This script requires nightly Rust, rustfmt, clippy, and cargo-expand
+
+set -euo pipefail
+IFS=$'\n\t'
+
+error() {
+    echo "error: $*" >&2
+}
+warn() {
+    echo "warning: $*" >&2
+}
+
+# Decide Rust toolchain. Nightly is used by default.
+toolchain="+nightly"
+if [[ "${1:-}" == "+"* ]]; then
+    toolchain="${1}"
+    shift
+fi
+# Make sure toolchain is installed.
+if ! cargo "${toolchain}" -V &>/dev/null; then
+    rustup toolchain install "${toolchain#+}" --no-self-update --profile minimal
+fi
+
+if [[ "${toolchain:-+nightly}" != "+nightly"* ]]; then
+    error "ci.sh requires nightly Rust"
+    exit 1
+fi
+if ! rustup "${toolchain}" component add rustfmt &>/dev/null ||
+    ! cargo expand -V &>/dev/null; then
+    warn "ci.sh requires rustfmt and cargo-expand to run all tests"
+fi
+
+# Run rustfmt.
+if ! rustup "${toolchain}" component add rustfmt &>/dev/null; then
+    warn "component 'rustfmt' is unavailable for toolchain '${toolchain#+}'"
+else
+    (
+        set -x
+        cargo "${toolchain}" fmt --all
+    )
+fi
+
+# This branch no longer actively developed. Most commits to this
+# branch are backporting and should not be blocked by clippy.
+# Run clippy.
+# if ! rustup "${toolchain}" component add clippy &>/dev/null; then
+#     warn "component 'clippy' is unavailable for toolchain '${toolchain#+}'"
+# else
+#     (
+#         set -x
+#         cargo "${toolchain}" clippy --all --all-features --all-targets -Z unstable-options
+#     )
+# fi
+
+set -x
+
+# Build documentation.
+cargo "${toolchain}" doc --no-deps --all --all-features
+
+# Run tests.
+cargo "${toolchain}" test --all --all-features
