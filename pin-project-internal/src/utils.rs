b229b57c8bd596f688a47304c708c0c864f7bc60
Taiki Endo <te316e89@gmail.com>
2021-08-24 23:10:05 +0900

    Reduce instantiations of syn::Error::new_spanned

--- pin-project-internal/src/utils.rs
+++ pin-project-internal/src/utils.rs
@@ -16,8 +16,8 @@ use syn::{
 pub(crate) type Variants = Punctuated<Variant, Token![,]>;
 
 macro_rules! format_err {
-    ($span:expr, $msg:expr) => {
-        syn::Error::new_spanned(&$span, $msg)
+    ($span:expr, $msg:expr $(,)?) => {
+        syn::Error::new_spanned(&$span as &dyn quote::ToTokens, &$msg as &dyn std::fmt::Display)
     };
     ($span:expr, $($tt:tt)*) => {
         format_err!($span, format!($($tt)*))
