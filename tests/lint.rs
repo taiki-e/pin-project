09c3e6aa1aa486f9e05ec577d959bdfada79ee94
Taiki Endo <te316e89@gmail.com>
2022-05-25 20:23:55 +0900

    Ignore clippy::undocumented_unsafe_blocks lint in tests

--- tests/lint.rs
+++ tests/lint.rs
@@ -239,14 +239,17 @@ pub mod basic {
                     Unit,
                 }
 
+                #[allow(clippy::undocumented_unsafe_blocks)]
                 unsafe impl<T: ::pin_project::__private::Unpin, U: ::pin_project::__private::Unpin>
                     ::pin_project::UnsafeUnpin for UnsafeUnpinStruct<T, U>
                 {
                 }
+                #[allow(clippy::undocumented_unsafe_blocks)]
                 unsafe impl<T: ::pin_project::__private::Unpin, U: ::pin_project::__private::Unpin>
                     ::pin_project::UnsafeUnpin for UnsafeUnpinTupleStruct<T, U>
                 {
                 }
+                #[allow(clippy::undocumented_unsafe_blocks)]
                 unsafe impl<T: ::pin_project::__private::Unpin, U: ::pin_project::__private::Unpin>
                     ::pin_project::UnsafeUnpin for UnsafeUnpinEnum<T, U>
                 {
