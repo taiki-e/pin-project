74c4485030e8eea223005547ff1945adbf63dc0b
Taiki Endo <te316e89@gmail.com>
2022-08-01 21:19:53 +0900

    Revert "Fix clippy::assertions_on_result_states warning"
    This reverts commit 36bf2d62f86520099f43834b8547d2aba0b7a479.

--- tests/pin_project.rs
+++ tests/pin_project.rs
@@ -861,7 +861,7 @@ fn project_replace_panic() {
         // called, so this is unreachable.
         unreachable!();
     }));
-    res.unwrap_err();
+    assert!(res.is_err());
     assert!(a);
     assert!(b);
     assert!(c);
@@ -879,7 +879,7 @@ fn project_replace_panic() {
         }
         unreachable!();
     }));
-    res.unwrap_err();
+    assert!(res.is_err());
     assert!(a);
     assert!(b);
     assert!(c);
