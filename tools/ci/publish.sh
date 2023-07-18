8be69b94fe8f3c59693c33ed1f2cf635edcfdc93
Taiki Endo <te316e89@gmail.com>
2023-07-19 04:38:57 +0900

    Move CI script from ci to tools/ci

--- tools/ci/publish.sh
+++ tools/ci/publish.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 set -euo pipefail
 IFS=$'\n\t'
-cd "$(dirname "$0")"/..
+cd "$(dirname "$0")"/../..
 
 # shellcheck disable=SC2154
 trap 's=$?; echo >&2 "$0: error on line "${LINENO}": ${BASH_COMMAND}"; exit ${s}' ERR
