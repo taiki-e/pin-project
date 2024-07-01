41d897be2f0d4f283eb501030574d3b314ba8a79
Taiki Endo <te316e89@gmail.com>
2024-07-02 00:35:46 +0900

    Ignore more clippy lints at workspace level

--- tests/lint/lib.rs
+++ tests/lint/lib.rs
@@ -65,7 +65,7 @@
     clippy::single_call_fn,
     clippy::single_char_lifetime_names
 )] // TODO
-#![allow(clippy::lint_groups_priority)] // https://github.com/rust-lang/rust-clippy/issues/12270
+#![allow(clippy::lint_groups_priority)] // https://github.com/rust-lang/rust-clippy/issues/12920
 
 /// Test for basic cases.
 pub mod basic {
